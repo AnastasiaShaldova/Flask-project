5b9c584c9d3752e2a9d123bb6e6d94d937c635c5
Анастасия <Ambrella915@gmail.com>
2023-03-26 15:15:15 +0300

    [+] миграции

--- wsgi.py
+++ wsgi.py
@@ -10,15 +10,17 @@ app.run(
     debug=True,
 )
 
+
 @app.cli.command("init-db")
 def init_db():
     db.create_all()
     print("БД создана")
 
+
 @app.cli.command("create-users")
 def create_users():
     from blog.models import Users
-    admin = Users(username="admin", email="admin@admin.ru", password=generate_password_hash('123456'),  is_staff=True)
+    admin = Users(username="admin", email="admin@admin.ru", password=generate_password_hash('123456'), is_staff=True)
     james = Users(username="james", email="james@james.ru", password=generate_password_hash('123456'), is_staff=False)
     db.session.add(admin)
     db.session.add(james)
